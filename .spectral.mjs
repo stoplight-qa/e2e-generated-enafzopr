482250263fbf208359923486175d78b822727c6a
qa-bcifc-0-ntfm@example.com <qa-bcifc-0-ntfm@example.com>
2023-05-11 14:14:01 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjIwMjQ1OQ/spectral.js?branch_slug=main&token=c9ef04fe-a9bf-477a-94da-10c6c777d01f";
+export default { extends: ruleset };
